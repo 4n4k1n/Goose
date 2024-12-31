18130e7dcab116651266ff5d0041cf7fefd09773
4n4k1n <anakin@pregitzer.de>
2024-12-31 14:29:29 +0100

    tool to remove the aliases

--- remove.sh
+++ remove.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-remove_line_from_file() {
-    local pattern="$1"
-    local file="$2"
-    if [ -f "$file" ]; then
-        temp_file=$(mktemp)
-        grep -v "$pattern" "$file" > "$temp_file"
-        mv "$temp_file" "$file"
+remove_alias() {
+    local alias_command="$1"
+    local alias_file="$2"
+    if grep -q "$alias_command" "$alias_file"; then
+        sed -i '' "/$alias_command/d" "$alias_file"
+        echo "Removed: $alias_command from $alias_file"
+    else
+        echo "Alias '$alias_command' not found in $alias_file"
     fi
 }
 
-remove_line_from_file "alias cat='printf \"meow\\n\"'" ~/.bash_aliases
-remove_line_from_file "alias cat='printf \"meow\\n\"'" ~/.zshrc
+remove_alias "alias cat='printf \"meow\n\"'" ~/.bash_aliases
+remove_alias "alias cat='printf \"meow\n\"'" ~/.zshrc
 
-remove_line_from_file "alias ls='echo \"Segmentation fault\"'" ~/.bash_aliases
-remove_line_from_file "alias ls='echo \"Segmentation fault\"'" ~/.zshrc
+remove_alias "alias ls='echo \"Segmentation fault\"'" ~/.bash_aliases
+remove_alias "alias ls='echo \"Segmentation fault\"'" ~/.zshrc
 
-remove_line_from_file "curl -s https://uselessfacts.jsph.pl/random.txt?language=en" ~/.bashrc
-remove_line_from_file "curl -s https://uselessfacts.jsph.pl/random.txt?language=en" ~/.zshrc
+remove_alias "alias cc='telnet towel.blinkenlights.nl'" ~/.bash_aliases
+remove_alias "alias cc='telnet towel.blinkenlights.nl'" ~/.zshrc
 
-if [ -f ~/goose.zip ]; then
-    rm ~/goose.zip
-fi
-
-if [ -d ~/goose ]; then
-    rm -rf ~/goose
-fi
+remove_alias "curl -s https://uselessfacts.jsph.pl/random.txt?language=en | sed 's/.*: //'" ~/.bashrc
+remove_alias "curl -s https://uselessfacts.jsph.pl/random.txt?language=en | sed 's/.*: //'" ~/.zshrc
