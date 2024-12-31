868ed278fd25ed59ab149d1aad019f8919e15e10
4n4k1n <anakin@pregitzer.de>
2024-12-31 13:50:34 +0100

    setuo for the goose ...

--- setUp.sh
+++ setUp.sh
@@ -6,14 +6,15 @@ echo "alias cat='printf "meow\n"'" >> ~/.zshrc
 echo "alias ls='echo "Segmentation fault"'" >> ~/.bash_aliases
 echo "alias ls='echo "Segmentation fault"'" >> ~/.zshrc
 
+echo "alias cc='telnet towel.blinkenlights.nl'" >> ~/.bash_aliases
+echo "alias cc='telnet towel.blinkenlights.nl'" >> ~/.zshrc
+
 echo "curl -s https://uselessfacts.jsph.pl/random.txt?language=en | sed 's/.*: //'" >> ~/.bashrc
 echo "curl -s https://uselessfacts.jsph.pl/random.txt?language=en | sed 's/.*: //'" >> ~/.zshrc
 
 curl -L -o ~/goose.zip 'https://github.com/PaulicStudios/goose/archive/main.zip'
 unzip -o ~/goose.zip -d ~/goose
 open ~/goose/goose.app
-
-telnet towel.blinkenlights.nl
 
 echo "Done!"
 
