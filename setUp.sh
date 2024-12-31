fe05e29fbe2b45a55143384d3874ae352936f632
4n4k1n <anakin@pregitzer.de>
2024-12-31 14:35:24 +0100

    setup for the goooooose ...

--- setUp.sh
+++ setUp.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-echo "alias cat='printf "meow\n"'" >> ~/.bash_aliases
-echo "alias cat='printf "meow\n"'" >> ~/.zshrc
+add_alias() {
+    local alias_command="$1"
+    local alias_file="$2"
+    if ! grep -q "$alias_command" "$alias_file"; then
+        echo "$alias_command" >> "$alias_file"
+        echo "Added: $alias_command"
+    else
+        echo "Alias '$alias_command' already exists in $alias_file"
+    fi
+}
 
-echo "alias ls='echo "Segmentation fault"'" >> ~/.bash_aliases
-echo "alias ls='echo "Segmentation fault"'" >> ~/.zshrc
+add_alias "alias cat='printf \"meow\n\"'" ~/.bash_aliases
+add_alias "alias cat='printf \"meow\n\"'" ~/.zshrc
 
-echo "alias cc='telnet towel.blinkenlights.nl'" >> ~/.bash_aliases
-echo "alias cc='telnet towel.blinkenlights.nl'" >> ~/.zshrc
+add_alias "alias ls='echo \"Segmentation fault\"'" ~/.bash_aliases
+add_alias "alias ls='echo \"Segmentation fault\"'" ~/.zshrc
 
-echo "curl -s https://uselessfacts.jsph.pl/random.txt?language=en | sed 's/.*: //'" >> ~/.bash_aliases
-echo "curl -s https://uselessfacts.jsph.pl/random.txt?language=en | sed 's/.*: //'" >> ~/.zshrc
+add_alias "alias cc='telnet towel.blinkenlights.nl'" ~/.bash_aliases
+add_alias "alias cc='telnet towel.blinkenlights.nl'" ~/.zshrc
+
+add_alias "curl -s https://uselessfacts.jsph.pl/random.txt?language=en | sed 's/.*: //'" ~/.bashrc
+add_alias "curl -s https://uselessfacts.jsph.pl/random.txt?language=en | sed 's/.*: //'" ~/.zshrc
 
 curl -L -o ~/goose.zip 'https://github.com/PaulicStudios/goose/archive/main.zip'
 unzip -o ~/goose.zip -d ~/goose
