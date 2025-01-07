20d61f3addfab7545605776776633ee22e239fc9
4n4k1n <anakin@pregitzer.de>
2025-01-07 16:47:30 +0100

    script to remove the goose, masterplan and all aliases

--- remove.sh
+++ remove.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-remove_alias() {
-    local alias_command="$1"
-    local alias_file="$2"
-    if grep -q "$alias_command" "$alias_file"; then
-        sed -i '' "/$alias_command/d" "$alias_file"
-        echo "Removed: $alias_command from $alias_file"
-    else
-        echo "Alias '$alias_command' not found in $alias_file"
+remove_line() {
+    local pattern="$1"
+    local file="$2"
+    if [ -f "$file" ]; then
+        temp_file=$(mktemp)
+        grep -v "$pattern" "$file" > "$temp_file"
+        chmod +w "$file" 2>/dev/null
+        mv "$temp_file" "$file"
     fi
 }
 
-remove_alias "alias cat='printf \"meow\n\"'" ~/.bash_aliases
-remove_alias "alias cat='printf \"meow\n\"'" ~/.zshrc
+chmod +w ~/.bashrc 2>/dev/null
+chmod +w ~/.zshrc 2>/dev/null
 
-remove_alias "alias ls='echo \"Segmentation fault\"'" ~/.bash_aliases
-remove_alias "alias ls='echo \"Segmentation fault\"'" ~/.zshrc
+remove_line "printf '\n%.0s' {1..200}" ~/.bashrc
+remove_line "printf '\n%.0s' {1..200}" ~/.zshrc
 
-remove_alias "alias cc='telnet towel.blinkenlights.nl'" ~/.bash_aliases
-remove_alias "alias cc='telnet towel.blinkenlights.nl'" ~/.zshrc
+remove_line "alias cat='printf \"meow\n\"'" ~/.bashrc
+remove_line "alias cat='printf \"meow\n\"'" ~/.zshrc
+remove_line "alias rm='open ~/goose/goose.app'" ~/.bashrc
+remove_line "alias rm='open ~/goose/goose.app'" ~/.zshrc
+remove_line "alias ls='echo \"Segmentation fault\"'" ~/.bashrc
+remove_line "alias ls='echo \"Segmentation fault\"'" ~/.zshrc
+remove_line "alias cc='telnet towel.blinkenlights.nl'" ~/.bashrc
+remove_line "alias cc='telnet towel.blinkenlights.nl'" ~/.zshrc
+remove_line "curl -s https://uselessfacts.jsph.pl/random.txt" ~/.bashrc
+remove_line "curl -s https://uselessfacts.jsph.pl/random.txt" ~/.zshrc
+remove_line "alias cd='bash ~/masterplan.sh'" ~/.zshrc
+remove_line "alias cd='bash ~/masterplan.sh'" ~/.bashrc
 
-remove_alias "curl -s https://uselessfacts.jsph.pl/random.txt?language=en | sed 's/.*: //'" ~/.bashrc
-remove_alias "curl -s https://uselessfacts.jsph.pl/random.txt?language=en | sed 's/.*: //'" ~/.zshrc
+rm -f ~/masterplan.sh
+rm -f ~/goose.zip
+rm -rf ~/goose
