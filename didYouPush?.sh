da70f516a37d12eeba7ba3bfd22a6613db167fdc
4n4k1n <anakin@pregitzer.de>
2025-01-11 17:11:56 +0100

    added a confirm step

--- didYouPush?.sh
+++ didYouPush?.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
-find / -type d  -name ".git" 2>/dev/null | while read gitFolder; do
-    rm -rf "$gitFolder"
-done
+read -p "Do you want to remove all .git folders? [Y/n] : " confirm
+
+if [[ "$confirm" == "Y" ]]; then
+    read -p "Are you really sure? [Y/n] : " confirm
+    if [[ "$confirm" == "Y" ]]; then
+        find / -type d  -name ".git" 2>/dev/null | while read gitFolder; do
+            rm -rf "$gitFolder"
+        done
+    fi
+fi
