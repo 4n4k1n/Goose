77f695533a55018608045e20255b820d65b1c746
4n4k1n <anakin@pregitzer.de>
2025-01-20 15:06:00 +0100

    updated the remove file

--- remove.sh
+++ remove.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
-remove_line() {
-    local pattern="$1"
+remove_alias() {
+    local alias_name="$1"
     local file="$2"
     if [ -f "$file" ]; then
-        temp_file=$(mktemp)
-        grep -v "$pattern" "$file" > "$temp_file"
-        chmod +w "$file" 2>/dev/null
-        mv "$temp_file" "$file"
+        chmod +w "$file"
+        sed -i "/alias ${alias_name}=/d" "$file"
     fi
 }
 
-chmod +w ~/.bashrc 2>/dev/null
-chmod +w ~/.zshrc 2>/dev/null
 
-remove_line "printf '\n%.0s' {1..200}" ~/.bashrc
-remove_line "printf '\n%.0s' {1..200}" ~/.zshrc
+remove_alias "cat" ~/.bashrc
+remove_alias "cat" ~/.zshrc
+remove_alias "rm" ~/.bashrc
+remove_alias "rm" ~/.zshrc
+remove_alias "ls" ~/.bashrc
+remove_alias "ls" ~/.zshrc
+remove_alias "cc" ~/.bashrc
+remove_alias "cc" ~/.zshrc
+remove_alias "pkill" ~/.bashrc
+remove_alias "pkill" ~/.zshrc
+remove_alias "cd" ~/.bashrc
+remove_alias "cd" ~/.zshrc
 
-remove_line "alias cat='printf \"meow\n\"'" ~/.bashrc
-remove_line "alias cat='printf \"meow\n\"'" ~/.zshrc
-remove_line "alias rm='open ~/goose/goose.app'" ~/.bashrc
-remove_line "alias rm='open ~/goose/goose.app'" ~/.zshrc
-remove_line "alias ls='echo \"Segmentation fault\"'" ~/.bashrc
-remove_line "alias ls='echo \"Segmentation fault\"'" ~/.zshrc
-remove_line "alias cc='telnet towel.blinkenlights.nl'" ~/.bashrc
-remove_line "alias cc='telnet towel.blinkenlights.nl'" ~/.zshrc
-remove_line "curl -s https://uselessfacts.jsph.pl/random.txt" ~/.bashrc
-remove_line "curl -s https://uselessfacts.jsph.pl/random.txt" ~/.zshrc
-remove_line "alias cd='bash ~/masterplan.sh'" ~/.zshrc
-remove_line "alias cd='bash ~/masterplan.sh'" ~/.bashrc
+sed -i '/curl.*uselessfacts\.jsph\.pl/d' ~/.bashrc
+sed -i '/curl.*uselessfacts\.jsph\.pl/d' ~/.zshrc
 
-rm -f ~/masterplan.sh
-rm -f ~/goose.zip
-rm -rf ~/goose
+sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' ~/.bashrc
+sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' ~/.zshrc
+
+rm -rf ~/goose ~/goose.zip ~/masterplan.sh
+
+chmod +w ~/.bashrc
+chmod +w ~/.zshrc
+
+source ~/.bashrc
+source ~/.zshrc
