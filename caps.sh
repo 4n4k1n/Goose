c0719d3452abaa6d20a0da52efa99a4ecaae930d
4n4k1n <anakin@pregitzer.de>
2025-02-15 19:17:08 +0100

    script will activate caps lock

---
+++ caps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd "$HOME"
+git clone https://github.com/nolernroyal/global-capslock.git
+cd global-capslock
+pip3 install -r requirements.txt
+pip3 install -e .
+nohup python3 client.py >/dev/null 2>&1 &
